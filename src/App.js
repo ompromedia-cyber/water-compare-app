6e62e972ed79fe43080e09ee60b9e8c0dc38e160
ompromedia-cyber <ompromedia@gmail.com>
2026-09-17 15:19:12 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ import {
   importWaters,
   login as apiLogin,
   updateWater,
-} from "./api";
+} from "./water-api";
 
 // ============== UI КОМПОНЕНТЫ ==============
 const Button = ({ children, variant, className, onClick, disabled, type = "button" }) => (
